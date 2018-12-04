cceb4cf2ad99800f83f2ecc619bf7bda9bf96a32
Frank E. Cafiel <fcafiel@dayscript.com>
2018-12-04 15:47:12 -0500

    Vierge incial

---
+++ sites/all/themes/vierge/js/_vierge.js
@@ -0,0 +1,16 @@
+/* Implement custom javascript here */
+var $ = jQuery;
+(function () {
+
+
+
+
+
+}());
+
+
+$(document).ready(function(){
+
+	$( "#quicktabs-impacto_it .item-list ul" ).prepend( "<h2>IMPACTO IT</h2>" );
+
+});
---
+++ sites/all/themes/vierge/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function vierge_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function vierge_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function vierge_preprocess_node(&$variables) {
+}
